9a3ac8a8036eb3b56a9406c9bccbe712af0812c5
favjiw <mfavianj@gmail.com>
2025-07-05 23:00:52 +0700

    firebase init

---
+++ lib/Model/Student.dart
@@ -0,0 +1,31 @@
+class Student {
+  String id;
+  String name;
+  String email;
+  String phoneNumber;
+
+  Student({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.phoneNumber,
+  });
+
+  factory Student.fromJson(Map<String, dynamic> json) {
+    return Student(
+      id: json['id'],
+      name: json['name'],
+      email: json['email'],
+      phoneNumber: json['phoneNumber'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'phoneNumber': phoneNumber,
+    };
+  }
+}
